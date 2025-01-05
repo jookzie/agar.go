ab1157bc1a47dd3edea13c554c25e435e818c69a
Mertan Rasim <mertan.rasim@gmail.com>
2025-01-06 00:29:47 +0100

    add: cors

--- server/main.go
+++ server/main.go
@@ -10,9 +10,19 @@ func main() {
 	go handleBroadcasts(wss)
 
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
 		handleConnections(wss, w, r)
 	})
 
 	log.Println("WebSocket server started on ws://localhost:8080/ws")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
